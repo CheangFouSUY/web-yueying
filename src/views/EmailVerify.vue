f9adb13eb666144a6599ee6db0f181109523ec2f
70216002黄乐怡 <mikiwongly@hotmail.my>
2022-04-28 17:24:19 +0800

    email verify modified

--- src/views/EmailVerify.vue
+++ src/views/EmailVerify.vue
@@ -7,9 +7,15 @@
     </div>
     <h1 class="emailTitle">邮箱验证</h1>
     <div class="verifyBox">
-      <input type="text" v-model="email" placeholder="输入邮箱">
       <button id="sendCode" @click="SendCode">发送验证码</button>
-      <input type="text" v-model="verifyKey" placeholder="输入验证码">
+      <div class="inputBox">
+        <img class="icon" src="../../public/Email.svg" alt="email_icon">  
+        <input type="text" v-model="email" placeholder="输入邮箱">
+      </div>
+      <div class="inputBox">
+        <img class="icon" src="../../public/Key.svg" alt="key_icon">  
+        <input type="text" v-model="verifyKey" placeholder="输入验证码">
+      </div>
       <div class='changeLoginWay'>
         <button @click='QuesVerify'>密保问题验证</button>
         <span class='symbol'>|</span>
@@ -48,6 +54,7 @@ export default {
 </script>
 
 <style scoped>
+/* logo和标题 */
 #logo{
     width: 350px;
     margin: 80px auto 40px;
@@ -70,43 +77,61 @@ export default {
   /* outline: 1px black solid; */
     text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
+.emailTitle{
+  font-weight: 100;
+  color: #456268;
+  text-align: center;
+  letter-spacing: 0.5em;
+  text-indent: 0.5em;
+  margin: 0 auto 20px;
+}
 
+/* 输入框 */
+.inputBox{
+    height: 50px;
+    width: 400px;
+    margin: 0px auto 40px;
+    border-radius: 10px;
+    background-color: rgba(121, 163, 177, 0.4);
+  /* outline: 1px black solid; */
+}
+.icon{
+  display: block;
+  float: left;
+  width: 40px;
+  margin: 5px 10px;
+  /* outline: 1px black solid; */
+}
 input:focus{
-    background-color: rgba(121, 163, 177, 0.6);
     outline: none;
+}
+.inputBox:focus-within{
+    background-color: rgba(121, 163, 177, 0.6);
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
 }
-input:hover{
+.inputBox:hover{
     background-color: rgba(121, 163, 177, 0.6);
 }
 input{
     font-family: "Microsoft JhengHei", 微软正黑体, "Microsoft YaHei", 微软雅黑;
     display: inline-block;
-    margin: 0px auto 40px;
     height: 50px;
-    width: 370px;
-    padding-left: 20px;
+    width: 320px;
     border: none;
-    border-radius: 10px;
-    background-color: rgba(121, 163, 177, 0.4);
+    background: none;
     color: #456268;
     font-size: 20px;
-    vertical-align: middle;
-    box-sizing: border-box;
-    -webkit-border-sizing: border-box;
-    -moz-border-sizing: border-box;
 }
-
 #sendCode{
   height: 50px;
-  position: absolute;
-  margin-left: 10px;
+  float: right;
   font-size: 20px;
   color: #456268;
   border: none;
   background: none;
-  /* outline: 1px black solid; */
 }
+
+/* 跳转链接 */
 .changeLoginWay button{
     display: inline-block;
     font-size: 14px;
@@ -116,8 +141,7 @@ input{
     /* outline: 1px black solid; */
 }
 .changeLoginWay{
-    width: 370px;
-    /* display:inline-block; */
+    width: 390px;
     margin: -40px auto 10px;
     text-align: right;
     /* outline: 1px black solid; */
@@ -125,10 +149,16 @@ input{
 .changeLoginWay button:hover, #sendCode:hover{
     color: rgba(121, 163, 177, 0.6);
 }
+.symbol{
+  color: #456268;
+  font-weight: 100;
+  font-size: 14px;
+  margin: auto 2px;
+}
 
 #submit{
     height: 40px;
-    width: 370px;
+    width: 400px;
     margin: 25px auto;
     background-color: #79A3B1;
     border: none;
@@ -143,28 +173,13 @@ input{
     background-color: rgba(121, 163, 177, 0.6);
 }
 
-.symbol{
-  color: #456268;
-  font-weight: 100;
-  font-size: 14px;
-  margin: auto 2px;
-}
 
 button{
   font-family: "Microsoft JhengHei", 微软正黑体, "Microsoft YaHei", 微软雅黑;
   cursor: pointer;
 }
-
-.emailTitle{
-  font-weight: 100;
-  color: #456268;
-  text-align: center;
-  letter-spacing: 0.5em;
-  text-indent: 0.5em;
-  margin: 0 auto 20px;
-}
 .verifyBox{
-    width: 600px;
+    width: 650px;
     margin: 0 auto;
     text-align: center;
     font-size: 20px;
