4e93cefeb20a3a3bedcdfbafcf6674551d52946b
PikaTea <pqy138@gmail.com>
2022-05-02 21:33:05 +0800

    Install axios

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = 'http://123.12.123.12:8000';
 
 Vue.config.productionTip = false
 
