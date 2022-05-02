741678bbec5396802e02601fdb95980ac20df6ab
PikaTea <pqy138@gmail.com>
2022-05-02 21:10:51 +0800

    Group mainpage modify

--- src/views/GroupMainpage.vue
+++ src/views/GroupMainpage.vue
@@ -5,8 +5,83 @@
       <el-button icon="el-icon-s-flag">创建小组</el-button>
     </div>
   <div class="leaderboard">
-    <div class="books">
-    </div>
+    <el-row>
+      <el-col :span="7"><div class="grid-content bg-blue"><div class="title"><p id="title">图书</p><div id="line"></div></div>
+      <el-table class="table"
+      :data="leaderboardData"
+      style="width: 90%"
+      :header-cell-style="{background : '#D0E8F2', color : '#456268' }"
+      :cell-style="{background : '#D0E8F2' }"
+      :cell-class-name="cellCSS">
+      <el-table-column
+        type=index
+        label="排名"
+        width="50"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="小组名字"
+        width="200">
+      </el-table-column>
+      <el-table-column
+        prop="count"
+        label="成员数量"
+        align="center">
+      </el-table-column>
+    </el-table>
+    </div></el-col>
+      <el-col :span="7"><div class="grid-content bg-blue"><div class="title"><p id="title">影视</p><div id="line"></div></div>
+      <el-table class="table"
+      :data="leaderboardData"
+      style="width: 90%"
+      :header-cell-style="{background : '#D0E8F2', color : '#456268' }"
+      :cell-style="{background : '#D0E8F2' }"
+      :cell-class-name="cellCSS">
+      <el-table-column
+        type=index
+        label="排名"
+        width="50"
+        align="center">
+      </el-table-column>
+      <el-table-column 
+        prop="name"
+        label="小组名字"
+        width="200">
+      </el-table-column>
+      <el-table-column
+        prop="count"
+        label="成员数量"
+        align="center">
+      </el-table-column>
+    </el-table>
+      </div></el-col>
+      <el-col :span="7"><div class="grid-content bg-blue"><div class="title"><p id="title">其他</p><div id="line"></div></div>
+      <el-table class="table"
+      :data="leaderboardData"
+      style="width: 90%"
+      :header-cell-style="{background : '#D0E8F2', color : '#456268' }"
+      :cell-style="{background : '#D0E8F2' }"
+      :cell-class-name="cellCSS">
+      <el-table-column
+        type=index
+        label="排名"
+        width="50"
+        align="center">
+      </el-table-column>
+      <el-table-column 
+        prop="name"
+        label="小组名字"
+        width="200">
+      </el-table-column>
+      <el-table-column
+        prop="count"
+        label="成员数量"
+        align="center">
+      </el-table-column>
+    </el-table>
+      </div></el-col>
+    </el-row>
     <div class="dramas">
     </div>
     <div class="others">
@@ -32,9 +107,52 @@ export default {
     Create(){
       this.$router.push('/creategroup')
     },
-  }
+    cellCSS({row, rowIndex}){
+      if (rowIndex === 0) {
+        return 'cellCSS1';
+      }
+      if (rowIndex === 1) {
+        return 'cellCSS2';
+      }
+      return 'cellCSS3';
+    },
+  },
+  data() {
+        return {
+          leaderboardData: [{
+            name: '只爱书不爱你',
+            count: '99'
+          }, {
+            name: '我只“书”于你',
+            count: '88'
+          }, {
+            name: 'Tomgirl',
+            count: '77'
+          }, {
+            name: 'Tomas',
+            count: '66'
+          }, {
+            name: 'Tomy',
+            count: '99'
+          }, {
+            name: 'Tomeh',
+            count: '88'
+          }, {
+            name: 'Tomos',
+            count: '77'
+          }, {
+            name: 'Tomson',
+            count: '66'
+          }, {
+            name: 'Tomwhee',
+            count: '77'
+          }, {
+            name: 'Tommom',
+            count: '66'
+          }],
+        }
+      }
 }
-
 </script>
 
 <style scoped>
@@ -51,19 +169,19 @@ export default {
 }
 .leaderboard{
   background-color: #FCF8EC;
-  height: 520px;
+  height: 610px;
   width: 1376px;
   margin-left: 80px;
   padding-top: 20px;
   /* width: 1536px; */
 }
-.books{
+/* .books{
   background-color: #D0E8F2;
   width: 300px;
   height: 500px;
   margin: 0 0 0 50px;
   border-radius: 20px;
-}
+} */
 .showgroups{
   background-color: #FCF8EC;
   margin-top: 20px;
@@ -71,11 +189,69 @@ export default {
   height: 300px;
   width: 1376px;
   margin-left: 80px;
-
+}
+.title{
+  background-color: #E9FBFF;
+  width: 100px;
+  height: 50px;
+  margin-left: 140px;
+  border-radius: 0 0 10px 10px;
+}
+#title {
+  display: inline;
+  margin-left: 20px;
+  font-size: 28px;
+}
+#line{
+  background-color: #005773;
+  height: 2px;
+  width: 70px;
+  margin-left: 15px;
 }
 #footer{
   position: relative;
   height: 88px;
 }
+.el-row {
+  margin-bottom: 20px;
+  margin-left: 40px;
+  margin-right: 40px;
+}
+.el-col {
+  border-radius: 4px;
+  margin-left: 30px;
+  margin-right: 20px;
+}
+.bg-blue {
+  background-color: #D0E8F2;
+}
+.grid-content {
+  border-radius: 10px;
+  min-height: 600px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+.table{
+  margin-left: 20px;
+  margin-top: 10px;
+}
+</style>
 
+<style>
+.el-table .cellCSS1 {
+  color: #077C96;
+  font-size: 22px;
+  font-weight: bold;
+}
+.el-table .cellCSS3  {
+  color: #79A3B1;
+  font-weight: bold;
+}
+.el-table .cellCSS2 {
+  color: #0BA7CA;
+  font-size: 16px;
+  font-weight: bold;
+}
 </style>
